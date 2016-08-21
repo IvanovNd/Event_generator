46b374c5e80d12062c97c9e71bf0b08f7c1a015d
IvanovND <ivanovndspb@gmail.com>
2016-08-21 19:03:54 +0300

    change data format to UTC

--- src/main/java/event/json/DateAdapter.java
+++ src/main/java/event/json/DateAdapter.java
@@ -5,21 +5,32 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
+
 
 /**
- * Created by Николай on 20.08.2016.
+ * The type Date adapter.
  */
 public class DateAdapter extends TypeAdapter<Date> {
+    /**
+     * The Dateformat.
+     */
+    static final String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
+
     public Date read(JsonReader reader) throws IOException {
         throw new UnsupportedOperationException("read is not supported");
     }
 
     public void write(JsonWriter writer, Date value) throws IOException {
+        SimpleDateFormat format = new SimpleDateFormat(DATEFORMAT);
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
+
         if (value == null) {
-            writer.value(new Date(0L).toString());
+            writer.value(format.format(new Date(0L)));
             return;
         }
-        writer.value(value.toString());
+        writer.value(format.format(value));
     }
 }
